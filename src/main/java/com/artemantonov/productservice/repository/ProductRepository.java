4872d95cc12b14b2a041d28b26ff42918b8c96c8
Artem Antonov <artem@antonov,es>
2023-09-28 14:13:30 +0200

    Indicate the product model

--- src/main/java/com/artemantonov/productservice/repository/ProductRepository.java
+++ src/main/java/com/artemantonov/productservice/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.artemantonov.productservice.repository;
 
+import com.artemantonov.productservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository {
+public interface ProductRepository extends MongoRepository<Product, String> {
+
 }
