200f8ac4f61dd3a88671b495e02665711cec1e50
Artem Antonov <artem@antonov,es>
2023-09-28 14:34:18 +0200

    Request Data Transfer Object and Product Service classes, invoke product service from controller.

---
+++ src/main/java/com/artemantonov/productservice/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.artemantonov.productservice.controller;
+
+import com.artemantonov.productservice.dto.ProductRequest;
+import com.artemantonov.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+
+    }
+}
