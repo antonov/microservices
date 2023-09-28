ac77080e5a3a72681561dd2bf71ef7a2dd9a0572
Artem Antonov <artem@antonov,es>
2023-09-28 14:01:21 +0200

    Initial commit

---
+++ src/main/java/com/artemantonov/productservice/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.artemantonov.productservice.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository {
+}
