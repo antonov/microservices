a0d01c6b61fc9df4f2519e8ffa7d9ef004efa91f
Artem Antonov <artem@antonov,es>
2023-10-10 14:13:18 +0200

    Name fix in the product response.

--- product-service/src/main/java/com/artemantonov/productservice/service/ProductService.java
+++ product-service/src/main/java/com/artemantonov/productservice/service/ProductService.java
@@ -36,6 +36,7 @@ public class ProductService {
     private ProductResponse mapToProductResponse(Product product) {
         return ProductResponse.builder()
                 .id(product.getId())
+                .name(product.getName())
                 .description(product.getDescription())
                 .price(product.getPrice())
                 .build();
