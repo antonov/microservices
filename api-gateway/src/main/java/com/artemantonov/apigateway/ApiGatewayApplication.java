49d121dbb71538cb2ab5e477b2ba3a6d866f97f3
Artem Antonov <artem@antonov,es>
2023-10-13 10:45:23 +0200

    Securing discovery server (eureka).

--- api-gateway/src/main/java/com/artemantonov/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/artemantonov/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.artemantonov.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,16 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+		serverHttpSecurity
+				.csrf().disable()
+				.authorizeExchange(exchange ->
+						exchange.pathMatchers("/eureka/**")
+								.permitAll()
+								.anyExchange()
+								.authenticated())
+				.oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+		return serverHttpSecurity.build();
+	}
 }
--- discovery-server/src/main/java/com/artemantonov/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/artemantonov/discoveryserver/DiscoveryServerApplication.java
@@ -3,14 +3,24 @@ package com.artemantonov.discoveryserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableWebSecurity
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+		return httpSecurity.build();
 	}
 
 }
